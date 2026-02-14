7abbdada9341a881b9a7d518e00fb6c28be87a4a
SmArT-32 <228971816+SmArT-32-projects@users.noreply.github.com>
2026-02-14 21:36:45 +0300

    Change version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,8 +10,8 @@ android {
         applicationId = "com.smart32.ambientdisplaydimmer"
         minSdk = 31
         targetSdk = 35
-        versionCode = 2
-        versionName = "1.1"
+        versionCode = 3
+        versionName = "1.2"
     }
 
     buildTypes {
